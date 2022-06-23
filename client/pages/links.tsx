9893361372e3a2f75684dae5680c7a33cf1c497f
azimut <azimut.github@protonmail.com>
2022-06-23 08:43:59 -0300

    links: pagination for empty q=

--- client/pages/links.tsx
+++ client/pages/links.tsx
@@ -11,7 +11,7 @@ type Resp = {
 
 function fetchBySearch(needle: string, page: number): string {
   if (needle === "") {
-    return "http://127.0.0.1:8080/links?page_size=30";
+    return `http://127.0.0.1:8080/links?page=${page}&page_size=30`;
   } else {
     return `http://127.0.0.1:8080/links?q=${encodeURIComponent(
       needle
