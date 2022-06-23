fe18b10da4c7fe4e4dc27e58e6d85c7386f5378a
azimut <azimut.github@protonmail.com>
2022-06-23 10:38:16 -0300

    links: backend refactor, use cors package

--- backend/go.mod
+++ backend/go.mod
@@ -3,6 +3,7 @@ module github.com/azimut/rounded-backend
 go 1.14
 
 require (
+	github.com/gin-contrib/cors v1.3.1 // indirect
 	github.com/gin-gonic/gin v1.8.1 // indirect
 	github.com/go-playground/validator/v10 v10.11.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
--- backend/main.go
+++ backend/main.go
@@ -3,37 +3,32 @@ package main
 import (
 	"net/http"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
 )
 
 var frontendUrl = "http://127.0.0.1:3000"
+var db *gorm.DB
 
 func main() {
+	db = InitDB()
+
 	r := gin.Default()
-	db := InitDB()
+	r.Use(cors.New(cors.Config{
+		AllowOrigins: []string{frontendUrl},
+	}))
 
 	r.GET("/ping", func(ctx *gin.Context) {
 		ctx.String(http.StatusOK, "pong")
 	})
 
-	r.GET("/message/:msgid", func(ctx *gin.Context) {
-		var rec Logs
-		ctx.Header("Access-Control-Allow-Origin", frontendUrl)
-		id := ctx.Params.ByName("id")
-		db.First(&rec, id)
-		if (Logs{}) == rec {
-			ctx.JSON(http.StatusNotFound, gin.H{})
-			return
-		}
-		ctx.JSON(http.StatusOK, rec)
-	})
-
 	r.GET("/messages/:id", func(ctx *gin.Context) {
 		var rec Logs
 		var recs []Logs
-		ctx.Header("Access-Control-Allow-Origin", frontendUrl)
 		id := ctx.Params.ByName("id")
 		db.First(&rec, id)
+
 		if (Logs{}) == rec {
 			ctx.JSON(http.StatusNotFound, gin.H{})
 			return
@@ -47,24 +42,35 @@ func main() {
 			Find(&recs)
 		ctx.JSON(http.StatusOK, recs)
 	})
-
-	r.GET("/links", func(ctx *gin.Context) {
-		var links []Link
-		ctx.Header("Access-Control-Allow-Origin", frontendUrl)
-		query := ctx.Request.URL.Query().Get("q")
-		if query == "" {
-			db.Order("msg_id desc").
-				Scopes(Paginate(ctx.Request)).
-				Find(&links)
-			ctx.JSON(http.StatusOK, links)
-		} else {
-			db.Where("link ILIKE ?", "%"+query+"%").
-				Order("msg_id desc").
-				Scopes(Paginate(ctx.Request)).
-				Find(&links)
-			ctx.JSON(http.StatusOK, links)
-		}
-	})
-
+	r.GET("/message/:msgid", handleGetMessage)
+	r.GET("/links", handleSearchLinks)
 	r.Run(":8080")
+}
+
+func handleGetMessage(ctx *gin.Context) {
+	id := ctx.Params.ByName("id")
+	var rec Logs
+	db.First(&rec, id)
+	if (Logs{}) == rec {
+		ctx.JSON(http.StatusNotFound, gin.H{})
+		return
+	}
+	ctx.JSON(http.StatusOK, rec)
+}
+
+func handleSearchLinks(ctx *gin.Context) {
+	var links []Link
+	query := ctx.Request.URL.Query().Get("q")
+	if query == "" {
+		db.Order("msg_id desc").
+			Scopes(Paginate(ctx.Request)).
+			Find(&links)
+		ctx.JSON(http.StatusOK, links)
+	} else {
+		db.Where("link ILIKE ?", "%"+query+"%").
+			Order("msg_id desc").
+			Scopes(Paginate(ctx.Request)).
+			Find(&links)
+		ctx.JSON(http.StatusOK, links)
+	}
 }
