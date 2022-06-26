eb0f3f3e0e9e3214638a5974340be508f6ad19e9
azimut <azimut.github@protonmail.com>
2022-06-26 09:28:17 -0300

    links: <Link> /message/id for each result

--- client/pages/links.tsx
+++ client/pages/links.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import Anchor from "components/Anchor";
 import Search from "components/Search";
 import useLinks from "hooks/useLinks";
+import Link from "next/link";
 
 export default function Links() {
   const [search, setSearch] = useState("");
@@ -23,9 +24,11 @@ export default function Links() {
       <div className="grid grid-cols-12 items-center gap-px bg-slate-100">
         {links.map((link) => (
           <>
-            <span className="col-span-1 text-xs text-gray-500 text-center truncate">
-              {link.MsgId}
-            </span>
+            <Link href={`message/${link.MsgId}`}>
+              <span className="col-span-1 text-xs text-gray-500 text-center truncate">
+                {link.MsgId}
+              </span>
+            </Link>
             <Anchor href={link.Link} otherClass="col-span-11 truncate" />
           </>
         ))}
